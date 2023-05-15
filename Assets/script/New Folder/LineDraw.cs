159e254111fc86d9b6cdc4424212f0ae2e14389b
moriya <21cge0021mk@edu.sca.ac.jp>
2023-05-15 10:52:40 +0900

    設定

--- Assets/script/New Folder/LineDraw.cs
+++ Assets/script/New Folder/LineDraw.cs
@@ -1,39 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class LineDraw : MonoBehaviour
 {
-    [SerializeField] private LineRenderer _rend;
-    [SerializeField] private Camera _cam;
-    [SerializeField] private EdgeCollider2D _collider;
-    private int posCount = 0;
-    private float interval = 0.1f;
-    private List<Vector2> _points = new List<Vector2>();
+
+    //線の材質
+    [SerializeField]
+    private Material lineMaterial;
+    //線の色
+    [SerializeField]
+    private Color lineColor;
+    //線の太さ
+    [Range(0.1f, 0.5f)]
+    [SerializeField]
+    private float lineWidth;
+
+    //線となるゲームオブジェクト用変数
+    private GameObject lineObj;
+    //lineObjのlineRenderer用変数
+    private LineRenderer lineRenderer;
+    //コライダーのための座標を保持するリスト型の変数
+    private List<Vector2> linePoints;
+
+    private void Start()
+    {
+        //Listの初期化
+        linePoints = new List<Vector2>();
+    }
+
     private void Update()
     {
-        Vector2 mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
+        //左クリックされたら
+        if (Input.GetMouseButtonDown(0))
+        {
+            _addLineObject();
+        }
+
+        //クリック中（ストローク中）
         if (Input.GetMouseButton(0))
-            SetPosition(mousePos);
-        else if (Input.GetMouseButtonUp(0))
-            posCount = 0;
+        {
+            _addPositionDataToLineRenderer();
+        }
     }
-    private void SetPosition(Vector2 pos)
+
+   //クリックしたら発動
+    void _addLineObject()
     {
-        if (!PosCheck(pos)) return;
-        posCount++;
-        _rend.positionCount = posCount;
-        _rend.SetPosition(posCount - 1, pos);
-        _points.Add(pos);
-        _collider.points = _points.ToArray();
+        //ゲームオブジェクト作成
+        lineObj = new GameObject();
+        //名前をLineにする
+        lineObj.name = "Line";
+        //lineObjにLineRendererコンポーネントを追加
+        lineObj.AddComponent<LineRenderer>();
+        //lineObjにEdgeCollider2Dコンポーネントを追加
+        lineObj.AddComponent<EdgeCollider2D>();
+        //lineObjを自身（Stroke）の子要素に設定
+        lineObj.transform.SetParent(transform);
+        _initRenderer();
     }
-    private bool PosCheck(Vector2 pos)
+
+    //lineObj初期化処理
+    void _initRenderer()
     {
-        if (posCount == 0) return true;
-        float distance = Vector2.Distance(_rend.GetPosition(posCount - 1), pos);
-        if (distance > interval)
-            return true;
-        else
-            return false;
+        //LineRendererを取得
+        lineRenderer = lineObj.GetComponent<LineRenderer>();
+        //ポジションカウントをリセット
+        lineRenderer.positionCount = 0;
+        //マテリアルを設定
+        lineRenderer.material = lineMaterial;
+        //マテリアルの色を設定
+        lineRenderer.material.color = lineColor;
+        Debug.Log("test");
+        //始点の太さを設定
+        lineRenderer.startWidth = lineWidth;
+        //終点の太さを設定
+        lineRenderer.endWidth = lineWidth;
+        //レイヤー指定
+        lineRenderer.renderingLayerMask = 2;
     }
+
+    void _addPositionDataToLineRenderer()
+    {
+        /*座標に関する処理*/
+        //マウス座標取得
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f);
+        //ワールド座標へ変換
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        /*LineRendererに関する処理*/
+        //LineRendererのポイントを増やす
+        lineRenderer.positionCount += 1;
+        //LineRendererのポジションを設定
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1, worldPos);
+
+        /*EdgeCollider2Dに関する処理*/
+        //ワールド座標をリストに追加
+        linePoints.Add(worldPos);
+        //EdgeCollider2Dのポイントを設定
+        lineObj.GetComponent<EdgeCollider2D>().SetPoints(linePoints);
+    }
+
 }
