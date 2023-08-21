26b42d9e45547e38cb8db94ea348b9ef2c982610
moriya <21cge0021mk@edu.sca.ac.jp>
2023-08-21 17:12:50 +0900

    音楽追加

--- Assets/script/ステージ移行保管用/GameOver.cs
+++ Assets/script/ステージ移行保管用/GameOver.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    //効果音用
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip sound1;
+
     //スプリクト用
     private TotalGM gm;
 
@@ -12,6 +17,7 @@ public class GameOver : MonoBehaviour
     void Start()
     {
         gm = FindObjectOfType<TotalGM>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,6 +28,7 @@ public class GameOver : MonoBehaviour
 
     public void ReloadStage()
     {
+        audioSource.PlayOneShot(sound1);
         gm.ReloadClearSchene();
     }
 }
