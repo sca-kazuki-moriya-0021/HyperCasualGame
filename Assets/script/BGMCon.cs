b69c7071c8d6e9a15644d83f6736967e51a768a4
moriya <21cge0021mk@edu.sca.ac.jp>
2023-08-22 14:35:24 +0900

    a

--- Assets/script/BGMCon.cs
+++ Assets/script/BGMCon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BGMCon : MonoBehaviour
 {
     private AudioSource audioSource;
+    //
     // Start is called before the first frame update
     void Start()
     {
