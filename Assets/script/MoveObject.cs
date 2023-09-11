c4a81d51588a64bd9245202d17276b29da2b39c0
moriya <21cge0021mk@edu.sca.ac.jp>
2023-09-11 14:41:08 +0900

    修正

--- Assets/script/MoveObject.cs
+++ Assets/script/MoveObject.cs
@@ -444,8 +444,10 @@ public class MoveObject : MonoBehaviour
         //二次元ベジェ曲線パターン
         //自分の位置
         var myP = transform.position;
+        var x = Mathf.Abs(hitCollider.bounds.min.x - Mathf.Abs(myP.x));
         //特定の位置
-        Vector3 toP =new Vector3(hitCollider.bounds.max.x + Mathf.Abs(myP.x),myP.y);
+        Vector3 toP =new Vector3(x,myP.y);
+        Debug.Log(toP.x);
         //中間の位置
         var miS = Mathf.Abs(hitCollider.bounds.size.y);
         Vector3 miP = new Vector3(hitCollider.bounds.center.x,miS + 5f);
