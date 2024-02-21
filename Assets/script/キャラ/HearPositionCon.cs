881feb5f29e6979025d624306639408e9d7f267b
moriya <21cge0021mk@edu.sca.ac.jp>
2024-02-21 13:13:07 +0900

    修正

--- Assets/script/キャラ/HearPositionCon.cs
+++ Assets/script/キャラ/HearPositionCon.cs
@@ -22,21 +22,27 @@ public class HearPositionCon : MonoBehaviour
 
         // 自身の座標
         var selfPosition = pos;
-        var frontPosition = selfPosition + Vector2.right; // 前方 向きに応じてrightを反転
-        var yAdjustMaxDistance = 0.5f; // 補正可能な高さ 
+        var frontPosition = selfPosition;
+        var yAdjustMaxDistance = 1.5f; // 補正可能な高さ 
         var rayOrigin = frontPosition + (Vector2.up * yAdjustMaxDistance); // Rayの発射位置
                                                                            // 自身の前方の少し上から真下に向けてRayを飛ばす
-        var castResult = Physics2D.Raycast(rayOrigin, Vector2.down, distance: yAdjustMaxDistance, 6);
+        var castResult = Physics2D.Raycast(rayOrigin, Vector2.down, distance: yAdjustMaxDistance);
+        Debug.DrawRay((Vector2)selfPosition, Vector2.down * yAdjustMaxDistance, Color.red);
+
         // 何とも命中しなかった場合
         if (castResult.collider == null)
+        {
+            Debug.Log("入ってる");
             return;
+        }
+
         // 命中したcolliderの法線と自身の上方向の内積を取って一定の傾き以下なら登れない or 下を向いた辺と判定
         var normal = Vector2.Dot(Vector2.up, castResult.normal);
         if (normal <= 0.25f)
             return;
 
         // 自身のy座標から補正するべき高さ
-        var yAdjustDistance = castResult.distance - yAdjustMaxDistance;
+        var yAdjustDistance = /*castResult.distance -*/ yAdjustMaxDistance;
         moveObject.transform.position = new Vector2(transform.position.x,transform.position.y + yAdjustDistance);
     }
 
